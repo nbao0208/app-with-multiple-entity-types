427ed5d86866671658b613557d52f7e57c4b0745
Bao Nguyen <baonguyen@Baos-MacBook-Pro.local>
2024-08-14 14:26:36 +0700

    feature/create project

---
+++ src/main/java/org/baonguyen/appwithmultipleenititiytypes/AppWithMultipleEnititiyTypesApplication.java
@@ -0,0 +1,13 @@
+package org.baonguyen.appwithmultipleenititiytypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithMultipleEnititiyTypesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppWithMultipleEnititiyTypesApplication.class, args);
+  }
+
+}
